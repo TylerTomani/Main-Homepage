e53da89395ccbf08822be7a2ba7013824b911ff0
Tyler Tomani <ttomani@gmail.com>
2024-11-01 12:58:59 -0400

    main

--- Projects/projects/languageApp-cssOnly/js/letterFocus.js
+++ Projects/projects/languageApp-cssOnly/js/letterFocus.js
@@ -1,19 +1,27 @@
 const translateSentence = document.querySelector('.translate-sentence textarea')
 const translation = document.querySelector('.translation textarea')
 
+let textareaFocus = false
 
 
 addEventListener('keydown', e => {
     let letter = e.key.toLowerCase()
-    if(letter == 't'){
-        console.log(translateSentence)
-        translateSentence.focus()
+    if(!textareaFocus){
+
+        if(letter == 't'){
+            console.log(translateSentence)
+            translateSentence.focus()
+        }
     }
     
 });
 [translateSentence, translation].forEach(el => {
-
     el.addEventListener('focus', e => {
         scrollTo(0, 0)
+        textareaFocus = true
+    })
+    el.addEventListener('focusout', e => {
+        scrollTo(0, 0)
+        textareaFocus = false
     })
 })
